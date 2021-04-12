d42c6c0dc7bf4375456ad390b44e5020f4a21913
João Vitor <joaovitorbaroni@outlook.com>
2021-04-11 21:32:44 -0300

    Adicionado novo pacote polished para tratamento de cores estilizado componente NewTransactionModal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,8 @@
 import Modal from 'react-modal';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import { Container, TransactionTypeContainer } from './styles';
+import closeImg from '../../assets/close.svg';
 
 interface NewTransactionModalProps {
   isModalOpen: boolean;
@@ -11,8 +15,42 @@ export function NewTransactionModal({isModalOpen, onRequestCloseModal}: NewTrans
     <Modal 
     isOpen={isModalOpen} 
     onRequestClose={onRequestCloseModal}
+    overlayClassName="react-modal-overlay"
+    className="react-modal-content"
   >
+    <button 
+      type="button" 
+      onClick={onRequestCloseModal}
+      className="react-modal-close"
+    >
+      <img src={closeImg} alt="Fechar modal"/>
+    </button>
+    <Container>
     <h2>Cadastrar transação</h2>
+
+    <input placeholder="Título"/>
+
+    <input placeholder="Valor" type="number"/>
+    <TransactionTypeContainer>
+      <button>
+        <img src={incomeImg} alt="Deposit" />
+        <span>Entrada</span>
+      </button>
+
+      <button>
+        <img src={outcomeImg} alt="Withdraw" />
+        <span>Saida</span>
+      </button>
+    </TransactionTypeContainer>
+
+    <input placeholder="Categoria"/>
+    <button type="submit">Cadastrar</button>
+
+
+
+    </Container>
+
+    
   </Modal>
 
   );
